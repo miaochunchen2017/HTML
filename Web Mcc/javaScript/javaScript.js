fae492226f71d25c18bd646f81d882f28c1dfb75
miaochunchen2017 <miaochunchen2011@163.coom>
2018-01-24 16:16:22 +0800

    2018-1-24 16:16:20

--- Web Mcc/javaScript/javaScript.js
+++ Web Mcc/javaScript/javaScript.js
@@ -1,5 +1,7 @@
 function turnOnTheBulb(){
+	//定位到img_turnOnTheBulb图片，将该图片元素赋予Element变量
 	Element = document.getElementById("img_turnOnTheBulb")
+	//匹配变量对应的元素名称（img/pic_bulbon.gif），如果存在"bulbon"则将变量图片更换为"img/pic_bulboff.gif"
 	if(Element.src.match("bulbon")){
 		Element.src = "img/pic_bulboff.gif"
 	}
