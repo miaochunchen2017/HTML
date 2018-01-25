f33745045b5c3d6d1ae73add52c231584db2a663
miaochunchen2017 <miaochunchen2011@163.coom>
2018-01-25 17:58:38 +0800

    2018-1-25 17:58:36

--- Web Mcc/javaScript/javaScript.js
+++ Web Mcc/javaScript/javaScript.js
@@ -3,9 +3,9 @@ function turnOnTheBulb(){
 	Element = document.getElementById("img_turnOnTheBulb")
 	//匹配变量对应的元素名称（img/pic_bulbon.gif），如果存在"bulbon"则将变量图片更换为"img/pic_bulboff.gif"
 	if(Element.src.match("bulbon")){
-		Element.src = "img/pic_bulboff.gif"
+		Element.src = "../img/pic_bulboff.gif"
 	}
 	else{
-		Element.src = "img/pic_bulbon.gif"
+		Element.src = "../img/pic_bulbon.gif"
 	}
 }
